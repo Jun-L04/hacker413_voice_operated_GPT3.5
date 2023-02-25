39fd817ce94ac34f94afbc08a75b5829c7e6156d
Junyang Lu <68449360+Jun-L04@users.noreply.github.com>
2023-02-25 15:46:00 -0500

    grouped code into method

--- chat.py
+++ chat.py
@@ -1,34 +1,32 @@
 import os
 import json
 import openai
-#openai is marked as not being used, but it is
-#solution was found here 
-#https://stackoverflow.com/questions/74311275/modulenotfounderror-no-module-named-openai
+# openai is marked as not being used, but it is
+# solution was found here
+# https://stackoverflow.com/questions/74311275/modulenotfounderror-no-module-named-openai
 
-#my OpenAI key (DO NOT LEAK)
+# my OpenAI key (DO NOT LEAK)
 openai.api_key = "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"
 print("ask away")
 
-#restricts response length
+# restricts response length
 usrInput = input() + "within 2 sentences"
 
-#TODO 
-#concatenate usrinput strings together so GPT knows context
-#maybe put it in an array and add each one
+# TODO
+# concatenate usrinput strings together so GPT knows context
+# maybe put it in an array and add each one
 
 
-#TODO
-#separate into different methods
+# TODO
+# separate into different methods
 
 
-#TODO
-#make this repetitive, don't stop after one iteration
+# TODO
+# make this repetitive, don't stop after one iteration
 
 
-
-
-#sets up an OpenAI completion model
-#gets a response from model as an OpenAIObject
+# sets up an OpenAI completion model
+# gets a response from model as an OpenAIObject
 response = openai.Completion.create(
     model="text-davinci-003",
     prompt=usrInput,
@@ -39,16 +37,15 @@ response = openai.Completion.create(
     presence_penalty=0.6
 )
 
-# converts OpenAIObject to json-formatted string
-modelResponseStr = json.dumps(response)
 
-#converts json string to python object having the same data strucutre
-pyModelObj = json.loads(modelResponseStr)
+def gpt_answer(response):
+    # converts OpenAIObject to json-formatted string
+    modelResponseStr = json.dumps(response)
+    # converts json string to python object having the same data strucutre
+    pyModelObj = json.loads(modelResponseStr)
+    # accesses the response from GPT
+    text_value = pyModelObj['choices'][0]['text']
+    return text_value
 
-#accesses the response from GPT
-text_value = pyModelObj['choices'][0]['text']
-
-#tests by printing to terminal
-print(text_value)
-
-
+# tests by printing to terminal
+print(gpt_answer(response))
