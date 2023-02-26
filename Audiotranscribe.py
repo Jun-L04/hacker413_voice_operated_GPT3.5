b721fa98d9e2afde87b09dcc21ce3a2786e3936a
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2023-02-25 21:05:28 -0500

    Create Audiotranscribe.py

---
+++ Audiotranscribe.py
@@ -0,0 +1,82 @@
+import json
+import os
+import boto3
+import pyttsx3
+import aws
+import time
+from urllib.parse import urlparse
+
+os.environ['AWS_ACCESS_KEY_ID'] ='AKIATONMT4U75CAEPIED'
+os.environ['AWS_SECRET_ACCESS_KEY']='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'
+
+
+transcribe = boto3.client('transcribe', region_name='us-east-2')
+s3 = boto3.client('s3')
+bucket_name = 'audiotest.01'
+file_name = '/Users/eric/Desktop/Test05.m4a'
+s3.upload_file(file_name, bucket_name, file_name)
+
+job_name = 'TEST48'
+job_uri = f's3://{bucket_name}/{os.path.basename(file_name)}'
+language_code = 'en-US'
+
+OutputKey= 'transcriptions/{}.json'.format(job_name)
+
+
+job_params = {
+    'TranscriptionJobName': job_name,
+    'Media': {
+        'MediaFileUri': f's3://{bucket_name}/{file_name}'
+    },
+    'OutputBucketName': 'voice-text-output',  # change this
+    'OutputKey': OutputKey,  # change this
+    'LanguageCode': language_code
+}
+
+transcribe.start_transcription_job(**job_params)
+
+while True:
+    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+        break
+    print("Waiting for job completion...")
+    time.sleep(10)
+
+bucket_name = 'voice-text-output'
+key = 'transcriptions/{}.json'.format(job_name)
+local_file_path = '/Users/eric/Downloads/{}.json'.format(job_name)
+
+if status['TranscriptionJob']['TranscriptionJobStatus'] == 'COMPLETED':
+    s3.download_file(bucket_name, key, local_file_path)
+    print("Output file downloaded successfully!")
+else:
+     print("Transcription job failed!")
+
+
+    
+
+
+'''folder_path = '/Users/eric/Downloads'
+files = os.listdir(folder_path)
+json_files = [f for f in files if f.endswith('.json')]
+
+n = len(json_files)-1'''
+
+filename = "/Users/eric/Downloads/{}.json".format(job_name)
+
+with open(filename,'r') as f:
+    data = json.load(f)
+
+a =','    
+list = data['results']['transcripts']
+output = list[0]['transcript']
+
+with open('/Users/eric/Downloads/text.txt','w') as f:
+    f.write(output)
+
+with open('/Users/eric/Downloads/text.txt',"r") as file:
+    answers = file.read()
+    
+engine = pyttsx3.init()
+engine.say(answers)
+engine.runAndWait()
